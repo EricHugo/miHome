27aef2558e9226a6c5a5db2f755805efe3000a7e
Eric Hugoson <eric@hugoson.org>
2022-07-18 20:15:36 +0200

    store people

--- miHome/learn_face.py
+++ miHome/learn_face.py
@@ -1,8 +1,27 @@
+#!/usr/bin/env python3
+
 import sys
+import json
+import pickle
 import cv2 
 import face_recognition
-import pickle
 
 
 if __name__ == "__main__":
     name = sys.argv[1]
+    try:
+        with open("people.json") as f:
+            people = json.load(f)
+        ref = 1 + int(max(list(people.keys())))
+    except FileNotFoundError:
+        people = {}
+        ref = 0
+    try:
+        with open("embeddings.pkl") as f:
+            embeddings = pickle.load(f)
+    except FileNotFoundError:
+        embeddings = {}
+
+    people[ref] = name
+    with open("people.json", "w") as outfile:
+        json.dump(people, outfile)
